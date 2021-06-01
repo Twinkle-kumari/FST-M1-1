287989a0c02d15c1e8871d80565aede4f9d00705
nikhilmhatre04 <84365829+nikhilmhatre04@users.noreply.github.com>
2021-06-01 10:35:09 +0530

    Add files via upload

---
+++ Python/Activities/Filereadtxt.py
@@ -0,0 +1,3 @@
+with open("C:\Users\NikhilMhatre\Desktop\SDET\SQL\Activity1.txt", "r") as file:
+  for line in file:
+    print(line)
---
+++ Python/Activities/sum.py
@@ -0,0 +1,5 @@
+result = 0
+numbers = [1,2,3,4,5]
+for number in numbers:
+result +=number
+print(numbers)
